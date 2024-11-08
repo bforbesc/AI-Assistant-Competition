08181c72d9843e4cd0ef30d77a46f478f662fb38
Martim Penim <127594074+martimpenim@users.noreply.github.com>
2024-11-08 18:11:26 +0100

    Update 0_Home.py

--- streamlit/0_Home.py
+++ streamlit/0_Home.py
@@ -1,1 +1,12 @@
+import streamlit as st
+#import streamlit_authenticator as stauth
 
+col1, col2= st.columns([6,1])  
+with col2:
+    sign_out = st.button('Sign Out')
+
+st.markdown("# AI Assistant Competition")
+st.sidebar.header("AI Assistant Competition")
+st.write(
+    """This is the home page of the app where we will briefly explain what is consists of."""
+)
